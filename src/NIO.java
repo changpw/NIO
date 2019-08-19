ae7961bcd4ffb0fe303e1629df29b22ab11f1564
changpw <1589273884@qq.com>
2019-08-19 19:14:42 +0800

    NIO And IO 修改

--- src/NIO.java
+++ src/NIO.java
@@ -1,14 +1,17 @@
 import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.Scanner;
 
 public class NIO {
 
     public static void main(String[] args) {
         try {
             long l = System.currentTimeMillis();
-            NIO();
+           // NIO();
            //IO2();
+           // mark();
+            Consl();
             long l1 =  System.currentTimeMillis();
             System.out.println(l1-l);
         }catch (Exception e){
@@ -20,8 +23,11 @@ public class NIO {
 * NIO
 * */
     public static void NIO() throws Exception{
-        FileInputStream fileInputStream = new FileInputStream(new File("E:\\jdk参考文档中文版.CHM"));
-        File file = new File("E:\\test.CHM");
+        FileInputStream fileInputStream = new FileInputStream(new File("E:\\test.txt"));
+        int available = fileInputStream.available();
+        System.out.println(available+"Byte");
+        fileInputStream.mark(10);
+        File file = new File("E:\\test1.txt");
         FileOutputStream fileOutputStream = new FileOutputStream(file);
         FileChannel fileInputStreamChannel = fileInputStream.getChannel();
         FileChannel fileOutputStreamChannel = fileOutputStream.getChannel();
@@ -60,6 +66,44 @@ public class NIO {
      }catch (IOException e){
          System.out.println( e.getMessage());
      }
+
+    }
+    /*
+    * mark标记
+    * */
+    public static void mark() throws IOException{
+        FileInputStream fileInputStream = new FileInputStream(new File("E:\\test1.txt"));
+        FileOutputStream fileOutputStream = new FileOutputStream(new File("E:\\test01.txt"));
+        byte [] buffer = new byte[10] ;
+        fileInputStream.mark(5);
+        int len =0 ;
+        while (( len = fileInputStream.read(buffer))!= -1){
+            System.out.println(fileInputStream.available());
+            if (fileInputStream.available()>1){
+                fileInputStream.reset();
+            }
+            fileOutputStream.write(buffer,0,len);
+        }
+        fileInputStream.close();
+        fileOutputStream.close();
+    }
+    /*
+    * 控制台输入
+    * */
+    public static void Consl() throws IOException{
+        FileWriter writer = new FileWriter(new File("E:\\test02.txt"),true);
+        while (true){
+            System.out.println("Please input text :");
+            Scanner scanner = new Scanner(System.in);
+            String line = scanner.nextLine();
+            if(null != line){
+                if(line.equals("exit")){
+                    System.exit(0);
+                }
+                writer.write(line);
+                writer.flush();
+            }
+        }
 
     }
 }
